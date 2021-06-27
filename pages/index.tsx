df255b7d5977a57edfc48f751fcc0b4ee9e4478b
ertrii <ertriilive@gmail.com>
2021-06-27 15:30:47 -0500

    :lipstick: section we are

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,6 @@
 import Footer from '../components/Footer'
 import Header from '../sections/home/Header'
+import WeAre from '../sections/home/WeAre'
 import Navbar from '../components/Navbar'
 import Contacts from '../sections/home/Contacts'
 
@@ -8,6 +9,7 @@ export default function Home() {
         <>
             <Navbar />
             <Header />
+            <WeAre />
             <Contacts />
             <Footer />
         </>
