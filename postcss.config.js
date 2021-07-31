63e1a38fd3f3e90edb559cc5ef497b98563154b1
ertrii <ertriilive@gmail.com>
2021-07-31 11:30:08 -0500

    :hammer: removed purgecss

--- postcss.config.js
+++ postcss.config.js
@@ -4,6 +4,7 @@ const purgecss = [
       content: [
           './pages/**/*.{js,jsx,ts,tsx}',
           './sections/**/*.{js,jsx,ts,tsx}',
+          './sections/**/**/*.{js,jsx,ts,tsx}',
           './components/**/*.{js,jsx,ts,tsx}'
       ],
       defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || []
@@ -14,7 +15,7 @@ module.exports = {
     plugins: [
         'postcss-easy-import',
         'tailwindcss',
-        process.env.NODE_ENV === 'production' ? purgecss : undefined,
+        // process.env.NODE_ENV === 'production' ? purgecss : undefined,
         'autoprefixer',
         'cssnano'
     ]
