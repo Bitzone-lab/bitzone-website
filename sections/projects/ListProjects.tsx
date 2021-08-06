e55da475cd093a1cd56da2c123eeccc3b0d40f70
Erik-GT-97 <erik.fisi16@gmail.com>
2021-08-06 18:14:33 -0500

    feat: update background service cloud and text type project

--- sections/projects/ListProjects.tsx
+++ sections/projects/ListProjects.tsx
@@ -1,6 +1,7 @@
 interface ContentProject {
     name: string
     description: string
+    type: string
     image: string
     icons: string[]
 }
@@ -9,6 +10,7 @@ const dataProjects: ContentProject[] = [
     {
         name: 'Inokuos',
         image: 'img/inokuos.png',
+        type: 'Proyecto Móvil',
         description:
             'Plataforma tecnológica real-time que gestiona rutinas de limpieza de principio a fin, manteniendo una comunicación activa e inmediata entre todos los actores involucrados en el proceso de limpieza',
         icons: [
@@ -21,6 +23,7 @@ const dataProjects: ContentProject[] = [
     {
         name: 'Advance MTR',
         image: 'img/mtr.png',
+        type: 'Proyecto Web',
         description:
             'Plataforma web real-time que monitorea en dashboards gráficos datos masivos enviados por sensores IoT. Los datos se procesan y almacenan en una base de datos de series de tiempo para agilizar la ingestión y su procesamiento',
         icons: [
@@ -35,6 +38,7 @@ const dataProjects: ContentProject[] = [
     {
         name: 'Aliados',
         image: 'img/aliados.png',
+        type: 'Proyecto Web',
         description:
             'Web app que recluta a Aliados para incrementar las ventas a través de referidos. Permite el registro y gestión de aliados, referidos  de pagos.',
         icons: [
@@ -46,6 +50,7 @@ const dataProjects: ContentProject[] = [
     {
         name: 'Procustodia',
         image: 'img/procustodia.jpg',
+        type: 'Proyecto Web',
         description:
             'Diseño y desarrollo del Website corporativo de Procustodia asociado a un Headless CMS que gestiona el contenido informativo, de oficinas y documentación e información legal',
         icons: ['img/netlify.svg', 'img/nextjs.svg', 'img/prismic.svg']
@@ -79,7 +84,10 @@ export default function ListProjects() {
                             }`}
                         >
                             <div className="xl:flex xl:justify-center xl:flex-col xl:items-center xl:h-full">
-                                <p className="font-sofia font-bold text-3xl text-subtle mt-8">
+                                <p className="text-tagline uppercase mt-8 hidden font-overpass-bold text-sm xl:block">
+                                    {dataProject.type}
+                                </p>
+                                <p className="font-sofia font-bold text-3xl text-subtle mt-8 xl:mt-5 xl:text-4xl">
                                     {dataProject.name}
                                 </p>
                                 <p className="font-sans font-light text-base text-normal mt-5">
--- tailwind.config.js
+++ tailwind.config.js
@@ -44,7 +44,7 @@ module.exports = {
                 'bits-img': 'url("/img/bits.png")',
                 'services-dev': 'url("/img/dev_bg.png")',
                 ux: 'linear-gradient(0deg, rgba(74, 37, 170, 0.5), rgba(74, 37, 170, 0.5)), url(/img/ux_bg.png)',
-                cloud: 'linear-gradient(0deg, rgba(74, 37, 170, 0.5), rgba(74, 37, 170, 0.5)), url(/img/cloud_bg.png)',
+                cloud: 'linear-gradient(0deg, rgba(74, 37, 170, 0.5), rgba(74, 37, 170, 0.5)), url(/img/cloud_bg.jpeg)',
                 dev: 'linear-gradient(0deg, rgba(74, 37, 170, 0.5), rgba(74, 37, 170, 0.5)), url(/img/dev_bg.png)',
                 about1: 'linear-gradient(0deg, rgba(74, 37, 170, 0.5), rgba(74, 37, 170, 0.5)), url(/img/about_1.png)',
                 about2: 'linear-gradient(0deg, rgba(74, 37, 170, 0.5), rgba(74, 37, 170, 0.5)), url(/img/about_2.png)',
