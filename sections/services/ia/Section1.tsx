70718f5f800fae8de5828e5f4f6f789701c0a2a8
ertrii <ertriilive@gmail.com>
2022-01-12 09:57:21 -0500

    :hammer: adjustment services ia

--- sections/services/ia/Section1.tsx
+++ sections/services/ia/Section1.tsx
@@ -7,19 +7,19 @@ export default function Section1() {
         <section>
             <div className="max-w-7xl w-full mx-auto py-24 flex items-center px-8 xl:px-0 flex-wrap sm:flex-nowrap text-center sm:text-left">
                 <div className="w-full sm:w-1/2">
+                    <h2 className="text-3xl font-sofia-semibold text-secondary mb-10">
+                        Customer Intelligence
+                    </h2>
+                    <p className="max-w-lg text-subtle font-overpass-light mx-auto sm:mx-0 mb-6 sm:mb-0">
+                        {t('Do you need to obtain...')}
+                    </p>
+                </div>
+                <div className="w-full sm:w-1/2">
                     <img
                         src="/images/services/user-idea.png"
                         alt="red-cloud"
                         className="mx-auto"
                     />
-                </div>
-                <div className="w-full sm:w-1/2">
-                    <h2 className="text-3xl font-sofia-semibold text-secondary mb-10">
-                        Customer Intelligence
-                    </h2>
-                    <p className="max-w-xl text-subtle font-overpass-light mx-auto sm:mx-0">
-                        {t('Do you need to obtain...')}
-                    </p>
                 </div>
             </div>
         </section>
